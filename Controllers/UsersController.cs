3f81e07bb64157031077c4334ae2e934cac6d5a8
Arnold <antaloczi.arnold@gmail.com>
2024-08-10 12:55:36 +0200

    Add export endpoint

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using UserServer.DTOs;
 using UserServer.Models;
 using UserServer.Services;
+using OfficeOpenXml;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UserServer.Controllers
 {
@@ -125,6 +128,58 @@ namespace UserServer.Controllers
             bool success = _usersService.DeleteUser(id);
 
             return success ? NoContent() : NotFound("Not Found");
+        }
+
+        /// <summary>
+        /// Export users to Excel
+        /// </summary>
+        [HttpGet("export")]
+        public IActionResult ExportUsersToExcel(
+            [FromQuery] int _page = 1,
+            [FromQuery] int _per_page = Int32.MaxValue,
+            [FromQuery] string _sort = "name")
+        {
+            _per_page = _per_page <= 0 ? 10 : _per_page;
+            _page = _page <= 0 ? 1 : _page;
+
+            // Fetching the users list without email filtering
+            List<UserDto>? users = _usersService.GetUsers(_page, _per_page, _sort).Data;
+
+            string? fileName = $"felhasznalok-{DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ss", CultureInfo.InvariantCulture)}.xlsx";
+
+            using ExcelPackage? package = new();
+
+            ExcelWorksheet? worksheet = package.Workbook.Worksheets.Add("Felhasználók");
+
+            // Adding Header
+            worksheet.Cells[1, 1].Value = "Név";
+            worksheet.Cells[1, 2].Value = "Email";
+            worksheet.Cells[1, 3].Value = "Kor";
+
+            // Making header bold
+            using (ExcelRange? range = worksheet.Cells[1, 1, 1, 3])
+            {
+                range.Style.Font.Bold = true;
+            }
+
+            // Adding user data
+            int row = 2;
+            foreach (UserDto? user in users)
+            {
+                worksheet.Cells[row, 1].Value = user.Name;
+                worksheet.Cells[row, 2].Value = user.Email;
+                worksheet.Cells[row, 3].Value = user.Age;
+                row++;
+            }
+
+            // AutoFit columns
+            worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+            // Convert the Excel package to a byte array
+            byte[]? excelData = package.GetAsByteArray();
+
+            // Return the file
+            return File(excelData, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
         }
     }
 }

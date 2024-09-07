75efdda450db0be26eb484ea0f651f1545c09c8f
Arnold <antaloczi.arnold@gmail.com>
2024-09-07 16:24:57 +0200

    Add migrations

--- Migrations/20240907142345_PreviousModifications.Designer.cs
+++ Migrations/20240907142345_PreviousModifications.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserServer.Data;
 
@@ -11,9 +12,11 @@ using UserServer.Data;
 namespace UserServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240907142345_PreviousModifications")]
+    partial class PreviousModifications
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
